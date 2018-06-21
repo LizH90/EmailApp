dcddcb1b5327869b0d9f057da561c284c90cc6de
Elizabeth Hard <elizabethhard@economist.com>
2018-06-21 16:09:18 +0100

    adding passport

---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+  
+  app.get('/auth/google/callback',
+    passport.authenticate('google')
+  )
