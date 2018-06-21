51fc0d24863824a535e421f5d0beb15ad3011aa2
Elizabeth Hard <elizabethhard@economist.com>
2018-06-21 16:19:32 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const passport 
+require('./services/passport');
 
 const app = express();
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,14 @@
+const passport = require('passport');
 
-
-app.get(
+module.exports = (app) => {
+  app.get(
     '/auth/google',
     passport.authenticate('google', {
       scope: ['profile', 'email']
     })
   );
-  
+
   app.get('/auth/google/callback',
     passport.authenticate('google')
   )
+};
